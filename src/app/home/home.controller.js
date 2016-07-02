99bacc3ad65c06903e3cf8ae2f3c1bc1834f5a65
Amy Benson <amy@yoomee.com>
2016-07-02 12:52:17 +0100

    moved tool to /tool and added homepage

---
+++ src/app/home/home.controller.js
@@ -0,0 +1,11 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('asylumjourneyFrontend')
+    .controller('HomeController', HomeController);
+
+  /** @ngInject */
+  function HomeController() {
+  }
+})();
