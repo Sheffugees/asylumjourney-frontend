f3d735fff6e49c923a8902febad785529c725f30
Clara Jordan <c.jordan@building-blocks.com>
2016-07-02 15:11:40 +0100

    permalink to individual cards

--- src/app/components/card/card.controller.js
+++ src/app/components/card/card.controller.js
@@ -5,11 +5,26 @@
   .module('asylumjourneyFrontend')
   .controller('CardController', CardController);
 
-    function CardController($scope, ngDialog) {
+    function CardController($scope, ngDialog, $routeParams, $rootScope) {
         var vm = this;
         vm.service = $scope.service;
+		vm.openDialog = openDialog;
 
-        vm.openDialog = function () {
+        activate();
+
+        function activate() {
+			checkRoute();
+        }
+
+		function checkRoute() {
+			// permalink to modal
+			if (!$rootScope.dialogOpen && (parseInt($routeParams.serviceId) === vm.service.id)) {
+				vm.openDialog();
+				$rootScope.dialogOpen = true;
+			}
+		}
+
+        function openDialog() {
             ngDialog.open({
                 template: 'app/components/detail-overlay/detail.html',
                 scope: $scope
