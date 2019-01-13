af457b2feed97d2f3df3e1971d17aed42d1106c9
amyvbenson <avbenson@gmail.com>
2019-01-13 15:22:35 +0000

    Added review dates and overdue reviews button to manage providers (#49)

--- src/app/components/providers/providers.js
+++ src/app/components/providers/providers.js
@@ -1,14 +1,16 @@
 import providersModal from './confirmModal.html';
 class providersController {
   /** @ngInject */
-  constructor (DataService, ngDialog, $scope, $timeout) {
+  constructor (DataService, ngDialog, $scope, $timeout, $filter) {
     this.DataService = DataService;
     this.ngDialog = ngDialog;
     this.$scope = $scope;
     this.$timeout = $timeout;
+    this.$filter = $filter;
     this.providers = [];
     this.showDeleteSuccess = false;
     this.idToDelete = 0;
+    this.order = 'name';
     getProviders.bind(this)();
   }
 
@@ -37,11 +39,31 @@ class providersController {
       }, 1000);
     });
   }
+
+  toggleShowReviewOverdueOnly () {
+    this.showReviewOverdueOnly = !this.showReviewOverdueOnly;
+    this.order = this.order === 'name' ? 'nextReviewDate' : 'name';
+  }
+}
+
+function formatNextReviewDate (providers) {
+  angular.forEach(providers, provider => {
+    if (!provider.nextReviewDate) {
+      return;
+    }
+    const now = new Date();
+    const nextReview = new Date(provider.nextReviewDate);
+    if (nextReview < now) {
+      provider.reviewDue = true;
+    }
+    provider.displayNextReviewDate = this.$filter('date')(provider.nextReviewDate, 'dd MMM yyyy')
+  });
+  return providers;
 }
 
 function getProviders () {
   this.DataService.getProviders().then( () => {
-    this.providers = angular.copy(this.DataService.dataStore.providers);
+    this.providers = formatNextReviewDate.bind(this)(angular.copy(this.DataService.dataStore.providers));
   });
 }
 
